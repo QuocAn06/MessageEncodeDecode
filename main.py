46b30c5d6b9adafa24e8bfa7e14558ca68234bd7
ADMIN\Admin <lenguyenquocan116@gmail.com>
2021-04-17 13:23:02 +0700

    add Function to decode

--- main.py
+++ main.py
@@ -42,6 +42,16 @@ def Encode(key, message):
     return base64.urlsafe_b64encode("".join(enc).encode()).decode()
 
 #?===========================Function to decode===============================
+def Decode(key, message):
+    dec = []
+    message = base64.urlsafe_b64decode(message).decode()
+
+    for i in range(len(message)):
+        key_c = key[i % len(key)]
+        dec.append(chr((256 + ord(message[i])-ord(key_c)) % 256))
+    
+    return "".join(dec)
+
 #?=========================Function to set mode===============================
 #?========================Function to exit window=============================
 #?========================Function to reset window============================
