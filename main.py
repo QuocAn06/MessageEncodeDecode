1d6dd62aea12cc51aa90117a61bb4eadd84ab704
ADMIN\Admin <lenguyenquocan116@gmail.com>
2021-04-17 15:19:11 +0700

    final

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import base64
 root = Tk()
 
 #Todo: set the width and height of the window
-root.geometry('500x600')
+root.geometry('500x400')
 #Todo: set the fixed size of the window
 root.resizable(0,0)
 #Todo: set the title of the window
@@ -36,8 +36,8 @@ def Encode(key, message):
     enc = []
 
     for i in range(len(message)):
-        key_c = key[i%len(key)]
-        enc.append(chr(ord(message[i]) + ord(key_c)) % 256)
+        key_c = key[i % len(key)]
+        enc.append(chr((ord(message[i]) + ord(key_c)) % 256))
 
     return base64.urlsafe_b64encode("".join(enc).encode()).decode()
 
@@ -70,5 +70,23 @@ def Reset():
     private_key.set("")
     _mode.set("")
     _result.set("")
-    
+
 #?==========================Labels and Buttons================================
+Label(root, font= ("Segoe UI",12,'bold'), text='Message: ').place(x= 60,y=60)
+Entry(root, font=("Segoe UI",10),textvariable=_text,bg='ghost white').place(x= 280,y=60)
+
+Label(root, font= ("Segoe UI",12,'bold'), text='Key: ').place(x= 60,y=90)
+Entry(root, font=("Segoe UI",10),textvariable=private_key,bg='ghost white').place(x= 280,y=90)
+
+Label(root, font= ("Segoe UI",12,'bold'), text='Mode(e-encode, d-decode): ').place(x= 60,y=120)
+Entry(root, font=("Segoe UI",10),textvariable=_mode,bg='ghost white').place(x= 280,y=120)
+
+Entry(root,font=("Segoe UI",10,'bold'),textvariable=_result,bg='ghost white').place(x= 280,y=150)
+
+Button(root,font=("Segoe UI",10,'bold'),text='RESULT',padx= 2, bg='LightGray',command=Mode).place(x= 60,y=150)
+
+Button(root, font = ("Segoe UI",10,'bold') ,text ='RESET' ,width =6, command = Reset,bg = 'LimeGreen', padx=2).place(x=100, y = 190)
+Button(root, font = ("Segoe UI",10,'bold') ,text= 'EXIT' , width = 6, command = Exit,bg = 'OrangeRed', padx=2, pady=2).place(x=200, y = 190)
+
+root.mainloop()
+
