0c1f145774e37eee3177021434f421033e239c3d
ADMIN\Admin <lenguyenquocan116@gmail.com>
2021-04-20 18:30:21 +0700

    edit size

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 #?===============================Import Libraries=============================
 #Todo: import tkinter module
 from tkinter import *
+from tkinter.scrolledtext import *
 
 #Todo: other necessery modules
 import base64
@@ -11,7 +12,7 @@ import base64
 root = Tk()
 
 #Todo: set the width and height of the window
-root.geometry('500x400')
+root.geometry('700x400')
 #Todo: set the fixed size of the window
 root.resizable(0,0)
 #Todo: set the title of the window
@@ -56,6 +57,7 @@ def Decode(key, message):
 def Mode():
     if(_mode.get() == 0):
         _result.set(Encode(private_key.get(),_text.get()))
+        Textbox.insert(0,"Hay")
     elif(_mode.get() == 1):
         _result.set(Decode(private_key.get(),_text.get()))
     else:
@@ -73,10 +75,12 @@ def Reset():
 
 #?==========================Labels and Buttons================================
 Label(root, font= ("Segoe UI",12,'bold'), text='Message: ').place(x= 60,y=60)
-Entry(root, font=("Segoe UI",10),textvariable=_text,bg='ghost white').place(x= 280,y=60)
+Entry(root,font=("Segoe UI",10),textvariable=_text,bg='ghost white').place(x= 280,y=60, 
+        height = 22, width = 350)
 
 Label(root, font= ("Segoe UI",12,'bold'), text='Key: ').place(x= 60,y=90)
-Entry(root, font=("Segoe UI",10),textvariable=private_key,bg='ghost white').place(x= 280,y=90)
+Entry(root, font=("Segoe UI",10),textvariable=private_key,bg='ghost white').place(x= 280,y=90,
+        height = 22, width = 350)
 
 #Label(root, font= ("Segoe UI",12,'bold'), text='Mode(e-encode, d-decode): ').place(x= 60,y=120)
 #Entry(root, font=("Segoe UI",10),textvariable=_mode,bg='ghost white').place(x= 280,y=120)
@@ -87,12 +91,16 @@ Radiobutton(root, font=("Segoe UI",10,'bold'), text="Encode", variable = _mode, 
 Radiobutton(root, font=("Segoe UI",10,'bold'), text="Decode", variable = _mode, value = 1,
             command = (_mode.get())).place(x = 380, y=120)
 
-Entry(root,font=("Segoe UI",10,'bold'),textvariable=_result,bg='ghost white').place(x= 280,y=150)
+Entry(root,font=("Segoe UI",10,'bold'),textvariable=_result,
+            bg='ghost white').place(x= 280, y=150, height = 50, width = 350,)
 
-Button(root,font=("Segoe UI",10,'bold'),text='RESULT',padx= 2, bg='LightGray',command=Mode).place(x= 60,y=150)
+Button(root,font=("Segoe UI",10,'bold'),text='RESULT',padx= 2,
+            bg='LightGray',command=Mode).place(x= 60, y=150)
 
-Button(root, font = ("Segoe UI",10,'bold') ,text ='RESET' ,width =6, command = Reset,bg = 'LimeGreen', padx=2).place(x=100, y = 190)
-Button(root, font = ("Segoe UI",10,'bold') ,text= 'EXIT' , width = 6, command = Exit,bg = 'OrangeRed', padx=2, pady=2).place(x=200, y = 190)
+Button(root, font = ("Segoe UI",10,'bold') ,text ='RESET' ,width =6, 
+            command = Reset,bg = 'LimeGreen', padx=2).place(x=280, y = 250)
+Button(root, font = ("Segoe UI",10,'bold') ,text= 'EXIT' , width = 6, 
+            command = Exit,bg = 'OrangeRed', padx=2, pady=2).place(x=380, y = 250)
 
 root.mainloop()
 
