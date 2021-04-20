c258a86ce83927533d86ad2656fbadc3ef52b98d
ADMIN\Admin <lenguyenquocan116@gmail.com>
2021-04-20 14:46:41 +0700

    import another class

--- _temp.py
+++ _temp.py
@@ -1,10 +1,20 @@
 
+
 #? Sử dụng thư viện tkinter 
 from tkinter import *
+from ClassVignere import *
 
 window = Tk()
 
 window.title("Welcome to Quoc An app")
+
+cVignere = CVignere("Have a nice day","ABC")
+
+cVignere.MaHoa()
+#Todo: tạo một label
+lbl = Label(window, text = cVignere.ciphertext)
+
+lbl.grid(column=0, row=0)
 
 #? mainloop() : vòng lập vô tận của window
 window.mainloop()
