dd82859a18a219fd3a5e62f9aa6a8bc8e655cbf6
ADMIN\Admin <lenguyenquocan116@gmail.com>
2021-04-21 22:25:44 +0700

    21APR2021: add combo box to app

--- _temp.py
+++ _temp.py
@@ -1,20 +1,40 @@
+import tkinter as tk
+from tkinter import ttk
+  
+# Creating tkinter window
+window = tk.Tk()
+window.geometry('350x250')
+# Label
+ttk.Label(window, text = "Select the Month :", 
+        font = ("Times New Roman", 10)).grid(column = 0, 
+        row = 15, padx = 10, pady = 25)
+  
+n = tk.StringVar()
+monthchoosen = ttk.Combobox(window, width = 27, 
+                            textvariable = n)
+  
+# Adding combobox drop down list
+monthchoosen['values'] = (' January', 
+                          ' February',
+                          ' March',
+                          ' April',
+                          ' May',
+                          ' June', 
+                          ' July', 
+                          ' August', 
+                          ' September', 
+                          ' October', 
+                          ' November', 
+                          ' December')
+  
+monthchoosen.grid(column = 1, row = 15)
+
+ttk.Label(window, text = "Select the Month :" + monthchoosen.get(), 
+        font = ("Times New Roman", 10)).grid(column = 0, 
+        row = 20, padx = 10, pady = 25)
 
 
-#? Sử dụng thư viện tkinter 
-from tkinter import *
-from ClassVignere import *
 
-window = Tk()
-
-window.title("Welcome to Quoc An app")
-
-cVignere = CVignere("Have a nice day","ABC")
-
-cVignere.MaHoa()
-#Todo: tạo một label
-lbl = Label(window, text = cVignere.ciphertext)
-
-lbl.grid(column=0, row=0)
-
-#? mainloop() : vòng lập vô tận của window
+# Shows february as a default value
+monthchoosen.current(1) 
 window.mainloop()
