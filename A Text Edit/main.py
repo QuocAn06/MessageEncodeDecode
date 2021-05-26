79a01d377915055423342252b8620497e95f87ed
ADMIN\Admin <lenguyenquocan116@gmail.com>
2021-05-26 23:24:59 +0700

    26/05/2021 - Create main Frame and Text Box

--- A Text Edit/main.py
+++ A Text Edit/main.py
@@ -7,7 +7,21 @@ root.title('My TextPad!')
 root.iconbitmap("c:/Users/Admin/Desktop/BT ATHTTT/_project/A Text Edit/image/alienware.ico")
 root.geometry("1000x660")
 
+#Create main Frame
+my_frame = Frame(root)
+my_frame.pack(pady=5)
 
+#Create our Scrollbar For the Text Box
+text_scroll = Scrollbar(my_frame)
+text_scroll.pack(side=RIGHT, fill=Y)
 
+#Create Text Box
+my_text = Text(my_frame, width= 88, height= 25, font=("Segoe UI", 16), 
+               selectbackground="blue", selectforeground="white", undo= True,
+               yscrollcommand=text_scroll.set)
+my_text.pack()
+
+#Configure our Scrollbar
+text_scroll.config(command= my_text.yview)
 
 root.mainloop()
